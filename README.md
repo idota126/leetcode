bfcb9e81ba1842f31829b72dfbfc30a2380836a3
idota126 <idota126@126.com>
2019-03-07 00:32:31 +0800

    Add PathSum

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ leetcode practice
        Also, post-order is widely use in mathematical expression. It is easier to write a program to parse a post-order expression. Here is an example:
        You can easily figure out the original expression using the inorder traversal. However, it is not easy for a program to handle this expression since you have to check the priorities of operations.
        
-       If you handle this tree in postorder, you can easily handle the expression using a stack. Each time when you meet a operator, you can just pop 2 elements from the stack, calculate the result and push the result back into the stack.
+       If you handle this datastructure.tree in postorder, you can easily handle the expression using a stack. Each time when you meet a operator, you can just pop 2 elements from the stack, calculate the result and push the result back into the stack.
 ##ArrayDeque
 查看ArrayDeque实现时候发现一段有意思的代码:
        
---
+++ src/main/java/leetcode/PathSum.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * @Description: https://leetcode.com/problems/path-sum/
+ * @Author idota
+ * @Date 2019/3/6
+ */
+public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        return isValid(root, 0, sum);
+    }
+
+    private boolean isValid(TreeNode child, int pretotal, int sum) {
+        if (child == null) {
+            return false;
+        }
+        //叶子节点 左右子节点都为空！！！
+        if(child.left==null&&child.right==null){
+            if(child.val+pretotal == sum ){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return isValid(child.left, child.val + pretotal, sum)
+                || isValid(child.right, child.val + pretotal,sum);
+    }
+
+}
